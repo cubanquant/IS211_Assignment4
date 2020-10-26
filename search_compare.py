4ded8d655d04a4683901622687c57d1a6a5666a2
cuban.quant <cuban.quant@gmail.com>
2020-10-25 22:35:53 -0400

    Create search_compare.py

---
+++ search_compare.py
@@ -0,0 +1,34 @@
+import argparse
+# other imports go here
+
+import random
+
+def get_me_random_list(n):
+    """Generate list of n elements in random order
+    
+    :params: n: Number of elements in the list
+    :returns: A list with n elements in random order
+    """
+    a_list = range(n)
+    random.shuffle(a_list)
+    return a_list
+    
+def sequential_search(a_list,item)::
+    pass
+
+
+def ordered_sequential_search(a_list,item)::
+    pass
+
+
+def binary_search_iterative(a_list,item)::
+    pass
+    
+    
+def binary_search_recursive(a_list,item)::
+    pass
+
+
+if __name__ == "__main__":
+    """Main entry point"""
+    pass
